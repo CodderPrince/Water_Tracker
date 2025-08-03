ea57ab5642e6270403559b4c17d1cc419bc72554
Md. An Nahian Prince <annahian44@gmail.com>
2025-08-04 02:42:49 +0600

    Create appBar.dart

---
+++ Water Tracker App1/water_tracker1/lib/appBar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+PreferredSizeWidget MyAppBar() {
+  return AppBar(
+    elevation: 8,
+    backgroundColor: Colors.transparent,
+    flexibleSpace: Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.blue, Colors.purple],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+
+        boxShadow: [
+          BoxShadow(color: Colors.black26, blurRadius: 8, offset: Offset(0, 4)),
+        ],
+      ),
+    ),
+
+    title: Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const SizedBox(width: 8),
+        ShaderMask(
+          shaderCallback:
+              (bounds) => const LinearGradient(
+                colors: [Colors.yellow, Colors.white],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ).createShader(Rect.fromLTWH(0, 0, bounds.width, bounds.height)),
+          child: const Text(
+            "Water Tracker",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    ),
+    centerTitle: true,
+    actions: [],
+  );
+}
